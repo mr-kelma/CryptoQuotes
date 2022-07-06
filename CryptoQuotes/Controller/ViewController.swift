612ac943fe0a904a038154da396041c93cc47b2d
Valery Keplin <98012564+mr-kelma@users.noreply.github.com>
2022-07-06 17:50:30 +0400

    Initial commit

---
+++ CryptoQuotes/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoQuotes
+//
+//  Created by Valery Keplin on 6.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
